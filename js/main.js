6617b5aa64ca8761979a6156c42abfba23f7f88a
k-strouse <kstrouse09@gmail.com>
2017-04-10 16:57:44 -0500

    Changes all over the place
    Commeneted out the list elements and a few other things, started messing
    with the JS code to implement a search box that calls on the Spotify
    Search API

--- js/main.js
+++ js/main.js
@@ -13,3 +13,22 @@ $('#search').hideseek({
 });
 
 /*   API Integration   */
+
+$(document).ready(function() {
+ $('form').submit(function (evt) {
+   evt.preventDefault();
+    var $searchField = $('#search');
+    var $submitButton = $('#submit');
+                          
+    $searchField.prop("disabled",true);
+    $submitButton.attr("disabled",true).val("searching...");
+    // the AJAX part
+    var spotifySearchAPI = "https://api.spotify.com/v1/search";
+    var spotifyAlbumAPI = "'https://api.spotify.com/v1/albums/' + albumId";
+    var searchTerms = $searchField.val();
+	});
+
+
+
+
+});
