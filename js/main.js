debccfa08e2021e2dceceb8c177af77394552052
k-strouse <kstrouse09@gmail.com>
2017-04-10 12:50:43 -0500

    Clone of Project 4
    Clone of project 4 because the layout/design and js functionality of
    this project's final layout will be similar

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*   Call lightGallery Plugin   */
+$("#lightgallery").lightGallery({
+  height: '400px',
+  download: false,
+  thumbnail: false,
+  counter: false,
+  mode: 'lg-lollipop',
+});
+
+/*   Call hideSeek Plugin   */
+$('#search').hideseek({
+  nodata: 'No Results Found',
+});
