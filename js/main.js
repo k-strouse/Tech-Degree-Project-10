17663f24c6725541daa1b749cf07468492eec2a8
k-strouse <kstrouse09@gmail.com>
2017-04-11 15:02:57 -0500

    Got the API working...now can't get the lighbox to work

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-/*   Call lightGallery Plugin   */
-$("#lightgallery").lightGallery({
-  height: '400px',
-  download: false,
-  thumbnail: false,
-  counter: false,
-  mode: 'lg-lollipop',
-});
 
 /*   Call hideSeek Plugin   */
 $('#search').hideseek({
@@ -24,11 +16,41 @@ $(document).ready(function() {
     $submitButton.attr("disabled",true).val("searching...");
     // the AJAX part
     var spotifySearchAPI = "https://api.spotify.com/v1/search";
-    var spotifyAlbumAPI = "'https://api.spotify.com/v1/albums/' + albumId";
-    var searchTerms = $searchField.val();
-	});
+    var searchTerms = $searchField.val();  //take the info the user inputs into the search box
+    var albumData = {
+      q: searchTerms,
+      type: 'album'
+    };
+    var albumID = [];  //blank album ID array
+    var spotifyAlbumAPI = '"https://api.spotify.com/v1/albums/" + albumId';
+    function displayAlbums(data) {                           //Function that gets all of the various album ID's associated with an artist the user has queried
+      var albumHTML = '<div id="lightGallery" class="gallery">';
+      $.each(data.albums.items,function(i,albums) {
+        albumID.push(data.albums.items[i].id);
+        //albumHTML += '<li data-src="' + data.albums.items[i].images[0].url + '" class="thumb1 image">';
+        albumHTML += '<a href="' + data.albums.items[i].images[0].url + '" class="image">';
+        albumHTML += '<img src="' + data.albums.items[i].images[1].url + '" class="thumbnails" /></a>';
+      });
+      albumHTML += "</div>";
+      $('#lightBoxDiv').html(albumHTML);      
+    }
+   $.getJSON(spotifySearchAPI, albumData, displayAlbums);
 
+    $searchField.prop("disabled",false);
+    $submitButton.attr("disabled",false).val("Search");
+	
+console.log(albumID);
 
-
-
+	});    
 });
+
+
+/*   Call lightGallery Plugin   */
+$("#lightGallery").lightGallery({
+  height: '400px',
+  download: false,
+  thumbnail: false,
+  counter: false,
+  mode: 'lg-lollipop',
+});
+
