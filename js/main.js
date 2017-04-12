1c5a93ae6268dc84fa514665a28880adbd83bef6
k-strouse <kstrouse09@gmail.com>
2017-04-12 13:21:07 -0500

    Added captions to lightGallery.js, styling changes
    Got the captions to work in lightGallery.js in regards to getting info
    from the API and populating associated fields. Working on some styling
    to get the responsive design down...home stretch!

--- js/main.js
+++ js/main.js
@@ -10,8 +10,7 @@ $(document).ready(function() {
  $('form').submit(function (evt) {
    evt.preventDefault();
     var $searchField = $('#search');
-    var $submitButton = $('#submit');
-                          
+    var $submitButton = $('#submit');                          
     $searchField.prop("disabled",true);
     $submitButton.attr("disabled",true).val("searching...");
     // the AJAX part
@@ -21,33 +20,33 @@ $(document).ready(function() {
       q: searchTerms,
       type: 'album'
     };
-    var albumID = [];  //blank album ID array
+    var albumID = new Array;  //blank album ID array
     var spotifyAlbumAPI = '"https://api.spotify.com/v1/albums/" + albumId';
     function displayAlbums(data) {                           //Function that gets all of the various album ID's associated with an artist the user has queried
       var albumHTML = '<div id="lightGallery" class="gallery">';
       $.each(data.albums.items,function(i,albums) {
         albumID.push(data.albums.items[i].id);
         //albumHTML += '<li data-src="' + data.albums.items[i].images[0].url + '" class="thumb1 image">';
-        albumHTML += '<a href="' + data.albums.items[i].images[0].url + '" class="image">';
-        albumHTML += '<img src="' + data.albums.items[i].images[1].url + '" class="thumbnails"></a>';
+        var captions = (i);
+        albumHTML += '<a href="' + data.albums.items[i].images[0].url + '" class="image" data-sub-html="#captions' + captions + '">';
+        albumHTML += '<img src="' + data.albums.items[i].images[1].url + '" class="thumbnails"/>';
+        albumHTML += '<div id="captions' + captions + '"><h2>Artist: ' + data.albums.items[i].artists[0].name + 
+        '</h2><h3>Title: ' + data.albums.items[i].name + '</h3>';
+        albumHTML += '</div></a>';        
       });
       albumHTML += '</div>'
       $('#lightBoxDiv').html(albumHTML);      
     }
    $.getJSON(spotifySearchAPI, albumData, displayAlbums);
-
     $searchField.prop("disabled",false);
-    $submitButton.attr("disabled",false).val("Search");
-	
+    $submitButton.attr("disabled",false).val("Search");	
 console.log(albumID);
-
 	});    
 });
 
 
 /*   Call lightGallery Plugin   */
 $(document).ajaxComplete(function () {    //This code requires all Ajax requests to be completed before the lightGallery plugin can work
-
     $("#lightGallery").lightGallery({
     height: '400px',
     download: false,
