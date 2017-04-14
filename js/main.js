98323638fe263b95c85e2c074390e918a23d55d1
k-strouse <kstrouse09@gmail.com>
2017-04-14 08:39:28 -0500

    Small changes to js AJAX requests
    Added a loop to loop through the album API array and perform a request
    on each album URL

--- js/main.js
+++ js/main.js
@@ -16,8 +16,43 @@ $(document).ready(function() {
       type: 'album'
     };
     var albumID = new Array;  //blank album ID array
+    var spotifyAlbumAPIarray = new Array;
+    var albumHTML = '<div id="lightGallery" class="gallery">';
     //var spotifyAlbumAPI = '"https://api.spotify.com/v1/albums/' + albumID[i] + '"';
-    function displayAlbums(data) {                           //Function that gets all of the various album ID's associated with an artist the user has queried
+    function getAlbumIDs(data) {                           //Function that gets all of the various album ID's associated with an artist the user has queried
+      $.each(data.albums.items,function(i,albums) {
+        albumID.push(data.albums.items[i].id);
+        var captions = (i);
+        var spotifyAlbumAPI = '"https://api.spotify.com/v1/albums/' + albumID[i] + '"';
+        albumHTML += '<a href="' + data.albums.items[i].images[0].url + '" class="image" data-sub-html="#captions' + captions + '">'; //Add album images for the lightbox & add class for captions
+        albumHTML += '<img src="' + data.albums.items[i].images[1].url + '" class="thumbnails"/>'; //Album thumbnails
+        albumHTML += '<div id="captions' + captions + '"><h2>Artist: ' + data.albums.items[i].artists[0].name + 
+        '</h2><h3>Album Title: ' + data.albums.items[i].name + '</h3>';  //These 2 lines of code add the div necessary for lightbox captions, the div has been removed in CSS styling
+        albumHTML += '</div></a>';     
+        var spotifyAlbumAPI = 'https://api.spotify.com/v1/albums/' + albumID[i] + '';
+        spotifyAlbumAPIarray.push(spotifyAlbumAPI);
+      });
+      albumHTML += '</div>';
+      $('#lightBoxDiv').html(albumHTML);
+      
+      
+      for (x = 0; x < spotifyAlbumAPIarray.length; x++) {  
+        $.getJSON(spotifyAlbumAPIarray[x]);
+        $("#captions").append('<h1>Artist:"' + data.album_type + '"</h1>');     //data.albums.items[i].images[0].url
+      }
+      
+    }
+
+    $.getJSON(spotifySearchAPI, albumData, getAlbumIDs);  //AJAX request
+    $searchField.prop("disabled",false);
+    $submitButton.attr("disabled",false).val("Search");	
+console.log(albumID);
+console.log (spotifyAlbumAPIarray);
+	});    
+});
+
+/*
+function getAlbumID(data) {                           //Function that gets all of the various album ID's associated with an artist the user has queried
       var albumHTML = '<div id="lightGallery" class="gallery">';
       $.each(data.albums.items,function(i,albums) {
         albumID.push(data.albums.items[i].id);
@@ -35,13 +70,13 @@ $(document).ready(function() {
       albumHTML += '</div>';
       $('#lightBoxDiv').html(albumHTML);  //Add the albumHTML to the appropriate div
     }
-   $.getJSON(spotifySearchAPI, albumData, displayAlbums);  //AJAX request
+   $.getJSON(spotifySearchAPI, albumData, getAlbumID);  //AJAX request
     $searchField.prop("disabled",false);
-    $submitButton.attr("disabled",false).val("Search");	
+    $submitButton.attr("disabled",false).val("Search"); 
 console.log(albumID);
-	});    
+  });    
 });
-
+*/
 
 /*   Call lightGallery Plugin   */
 $(document).ajaxComplete(function () {    //This code requires all Ajax requests to be completed before the lightGallery plugin can work
